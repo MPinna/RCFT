24a16665e1b1fbb677c75aecaaf2663b41ceb865
MPinna <marco.pinn95@gmail.com>
2020-04-06 20:09:12 +0200

    Include network libraries

--- source/include/util.h
+++ source/include/util.h
@@ -1,5 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
 
 int exists(const char* file);
