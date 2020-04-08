e518ad1d04a86e1b771a7a53e95132eb71b87b32
MPinna <marco.pinn95@gmail.com>
2020-04-08 19:44:58 +0200

    Remove debug print from exists function

--- source/include/util.c
+++ source/include/util.c
@@ -13,13 +13,9 @@ int exists(const char* file)
     FILE *fptr = fopen(file, "r");
     if(fptr)
     {
-        printf("%s exists\n", file);
         fclose(fptr);
         return 1;
     }
     else
-    {
-        printf("%s does not exists\n", file);
         return 0;
-    }
 }
